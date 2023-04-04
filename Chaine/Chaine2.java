02ba1e9109e87d02901b406eab4ad14541692071
chinawhwx <chinawhwx@gmail.com>
2023-04-04 17:52:48 +0200

    setCharAt() in StringBuilder and StringBuffer

--- Chaine/Chaine2.java
+++ Chaine/Chaine2.java
@@ -10,3 +10,15 @@ public class Chaine2 {
     System.out.println(sb.append(" ").append(s1));
   }
 }
+
+/*
+ *  StringBuilder sb = new StringBuilder("hello");
+    System.out.println("Before modification: " + sb);
+        
+    sb.setCharAt(0, 'H');
+    System.out.println("After modification: " + sb);
+ *
+ *  setCharAt() 方法可以在 StringBuilder 中使用，因为 StringBuilder 和 StringBuffer 类似，
+ *  都是可变字符串，它们都继承自 AbstractStringBuilder 类。
+ *  因此，StringBuilder 也具有 setCharAt() 方法。
+ */
